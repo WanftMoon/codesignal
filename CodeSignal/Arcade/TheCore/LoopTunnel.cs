dd995ef37a45bf213e0fcba2b3ec3d1cc0b7a542
WanftMoon <wanft@moonlighters.com.br>
2021-05-18 09:29:05 -0300

    loop tunnel

--- CodeSignal/Arcade/TheCore/LoopTunnel.cs
+++ CodeSignal/Arcade/TheCore/LoopTunnel.cs
@@ -418,5 +418,126 @@ namespace CodeSignal.Arcade.TheCore
         }
 
 
+        /// <remarks>
+        /// When a candle finishes burning it leaves a leftover. makeNew leftovers can be combined to make a new candle, which, when burning down, will in turn leave another leftover.
+        /// 
+        /// You have candlesNumber candles in your possession.What's the total number of candles you can burn, assuming that you create new candles as soon as you have enough leftovers?
+        /// 
+        /// Example
+        /// 
+        /// For candlesNumber = 5 and makeNew = 2, the output should be
+        /// candles(candlesNumber, makeNew) = 9.
+        /// 
+        /// Here is what you can do to burn 9 candles:
+        /// 
+        /// 
+        /// burn 5 candles, obtain 5 leftovers;
+        /// create 2 more candles, using 4 leftovers(1 leftover remains);
+        ///     burn 2 candles, end up with 3 leftovers;
+        /// create another candle using 2 leftovers(1 leftover remains);
+        ///     burn the created candle, which gives another leftover(2 leftovers in total);
+        ///     create a candle from the remaining leftovers;
+        /// burn the last candle.
+        /// Thus, you can burn 5 + 2 + 1 + 1 = 9 candles, which is the answer.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds (cs)
+        /// 
+        /// [input] integer candlesNumber
+        /// 
+        /// The number of candles you have in your possession.
+        /// 
+        /// Guaranteed constraints:
+        /// 1 ≤ candlesNumber ≤ 15.
+        /// 
+        /// [input] integer makeNew
+        /// 
+        /// The number of leftovers that you can use up to create a new candle.
+        /// 
+        /// Guaranteed constraints:
+        /// 2 ≤ makeNew ≤ 5.
+        /// 
+        /// [output] integer
+        /// </remarks>
+        public static int Candles(int candlesNumber, int makeNew)
+        {
+            int leftOvers = candlesNumber;
+            int candlesBurned = candlesNumber;
+
+            while (leftOvers >= makeNew)
+            {
+                candlesBurned += leftOvers / makeNew;
+                leftOvers = leftOvers / makeNew + leftOvers % makeNew;
+            }
+
+            return (candlesBurned);
+        }
+
+
+        /// <remarks>
+        /// Imagine a white rectangular grid of n rows and m columns divided into two parts by a diagonal line running from the upper left to the lower right corner. Now let's paint the grid in two colors according to the following rules:
+        /// 
+        /// A cell is painted black if it has at least one point in common with the diagonal;
+        /// Otherwise, a cell is painted white.
+        /// Count the number of cells painted black.
+        ///
+        /// Example
+        /// 
+        /// For n = 3 and m = 4, the output should be
+        /// countBlackCells(n, m) = 6.
+        /// 
+        /// 
+        /// There are 6 cells that have at least one common point with the diagonal and therefore are painted black.
+        /// For n = 3 and m = 3, the output should be
+        /// countBlackCells(n, m) = 7.
+        /// 
+        /// 7 cells have at least one common point with the diagonal and are painted black.
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds (cs)
+        /// 
+        /// [input] integer n
+        /// 
+        /// The number of rows.
+        /// 
+        /// Guaranteed constraints:
+        /// 1 ≤ n ≤ 105.
+        /// 
+        /// 
+        /// [input] integer m
+        /// 
+        /// 
+        /// The number of columns.
+        /// 
+        /// 
+        /// Guaranteed constraints:
+        /// 1 ≤ m ≤ 105.
+        /// 
+        /// 
+        /// [output] integer
+        /// 
+        /// The number of black cells.
+        /// </remarks>
+        public static int CountBlackCells(int n, int m)
+        {
+            return n + m + GCD(n, m) - 2;
+        }
+
+        /// <summary>
+        /// helper
+        /// </summary>
+        public static int GCD(int n, int m)
+        {
+            while (m > 0)
+            {
+                int temp = n;
+                n = m;
+                m = temp % m;
+            }
+
+            return n;
+        }
+
     }
 }
