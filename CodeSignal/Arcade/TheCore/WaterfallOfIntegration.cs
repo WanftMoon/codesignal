31390af6649ac51314c2113efe914b4f27ebe33f
WanftMoon <wanft@moonlighters.com.br>
2021-05-31 10:04:35 -0300

    sorting outpost

--- CodeSignal/Arcade/TheCore/WaterfallOfIntegration.cs
+++ CodeSignal/Arcade/TheCore/WaterfallOfIntegration.cs
@@ -512,5 +512,178 @@ namespace CodeSignal.Arcade.TheCore
                 .ToArray();
         }
 
+        /// <remarks>
+        /// Court is in session. We got a group of witnesses who have all taken an oath to tell the truth. The prosecutor is pointing at the defendants one by one and asking each witnesses a simple question - "guilty or not?". The witnesses are allowed to respond in one of the following three ways:
+        /// 
+        /// I am sure he/she is guilty.
+        /// I am sure he/she is innocent.
+        /// I have no idea.
+        /// The prosecutor has a hunch that one of the witnesses might not be telling the truth so she decides to cross-check all of their testimonies and see if the information gathered is consistent, i.e.there are no two witnesses A and B and a defendant C such that A says C is guilty while B says C is innocent.
+        /// 
+        /// Example
+        /// 
+        /// For
+        /// 
+        /// 
+        /// evidences = [[0, 1, 0, 1],
+        ///              [-1, 1, 0, 0],
+        ///              [-1, 0, 0, 1]]
+        /// the output should be
+        /// isInformationConsistent(evidences) = true;
+        /// 
+        /// For
+        /// 
+        /// evidences = [[1, 0],
+        ///              [-1, 0],
+        ///              [1, 1],
+        ///              [1, 1]]
+        /// the output should be
+        /// isInformationConsistent(evidences) = false.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds(cs)
+        /// 
+        /// [input]
+        ///         array.array.integer evidences
+        /// 
+        /// 2-dimensional array of integers representing the set of testimonials.evidences[i][j] is the testimonial of the ith witness on the jth defendant. -1 means "innocent", 0 means "no idea", and 1 means "guilty".
+        /// 
+        /// 
+        /// Guaranteed constraints:
+        /// 2 ≤ evidences.length ≤ 5,
+        /// 2 ≤ evidences[0].length ≤ 10,
+        /// evidences[i].length = evidences[j].length,
+        /// -1 ≤ evidences[i][j] ≤ 1.
+        /// 
+        /// 
+        /// [output] boolean
+        /// 
+        /// true if the evidence is consistent, false otherwise.
+        /// </remarks>
+        public static bool IsInformationConsistent(int[][] evidences)
+        {
+            for (int j = 0; j < evidences[0].Length; j++)
+            {
+                bool guilt = false;
+                bool innocent = false;
+
+                for (int i = 0; i < evidences.Length; i++)
+                {
+                    if (evidences[i][j] == -1)
+                        innocent = true;
+                    else if (evidences[i][j] == 1)
+                        guilt = true;
+
+                    if (guilt && innocent)
+                        return (false);
+                }
+            }
+
+            return (true);
+        }
+
+        /// <remarks>
+        /// A nonogram is also known as Paint by Numbers and Japanese Crossword. The aim in this puzzle is to color the grid into black and white squares. At the top of each column, and at the side of each row, there are sets of one or more numbers which describe the runs of black squares in that row/column in exact order. For example, if you see 10 1 along some column/row, this indicates that there will be a run of exactly ten black squares, followed by one or more white squares, followed by a single black square. The cells along the edges of the grid can also be white.
+        /// 
+        /// You are given a square nonogram of size size.Its grid is given as a square matrix nonogramField of size(size + 1) / 2 + size, where the first(size + 1) / 2 cells of each row and and each column define the numbers for the corresponding row/column, and the rest size × size cells define the the grid itself.
+        /// 
+        /// Determine if the given nonogram has been solved correctly.
+        /// 
+        /// Note: here / means integer division.
+        /// 
+        /// Example
+        /// 
+        /// For size = 5 and
+        /// 
+        /// nonogramField = [["-", "-", "-", "-", "-", "-", "-", "-"],
+        /// 
+        ///                  ["-", "-", "-", "2", "2", "1", "-", "1"],
+        /// 
+        ///                  ["-", "-", "-", "2", "1", "1", "3", "3"],
+        /// 
+        ///                  ["-", "3", "1", "#", "#", "#", ".", "#"],
+        /// 
+        ///                  ["-", "-", "2", "#", "#", ".", ".", "."],
+        /// 
+        ///                  ["-", "-", "2", ".", ".", ".", "#", "#"],
+        /// 
+        ///                  ["-", "1", "2", "#", ".", ".", "#", "#"],
+        /// 
+        ///                  ["-", "-", "5", "#", "#", "#", "#", "#"]]
+        /// the output should be correctNonogram(size, nonogramField) = true;
+        /// 
+        /// For size = 5 and
+        /// 
+        /// nonogramField = [["-", "-", "-", "-", "-", "-", "-", "-"],
+        ///                  ["-", "-", "-", "-", "-", "1", "-", "-"],
+        ///                  ["-", "-", "-", "3", "3", "2", "5", "5"],
+        ///                  ["-", "-", "3", ".", ".", ".", "#", "#"],
+        ///                  ["-", "2", "2", "#", "#", "#", "#", "#"],
+        ///                  ["-", "-", "5", "#", "#", "#", "#", "#"],
+        ///                  ["-", "-", "5", "#", "#", "#", "#", "#"],
+        ///                  ["-", "-", "2", ".", ".", ".", "#", "#"]]
+        /// the output should be correctNonogram(size, nonogramField) = false.
+        /// 
+        /// There are three mistakes in the nonogram:
+        /// 
+        /// In the 5th(1-based) row there are numbers["-", "2", "2"], so there should be two runs of 2 black squares separated by at least one white square.However, there is only one run of 5 black squares.
+        /// In the 6th column there are numbers["-", "1", "2"], so there should be a run of exactly 1 black square, followed by one or more white squares, followed by another 2 black squares. However, there is a single run of 3 black squares not separated by white ones.
+        /// Finally, in the 4th row there are numbers["-", "-", "3"], so there should be a single run of exactly 3 black squares. However, there is just a 2-square run in that row.
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds (cs)
+        /// 
+        /// [input] integer size
+        /// 
+        /// A positive integer, the size of the grid.
+        /// 
+        /// Guaranteed constraints:
+        /// 5 ≤ size ≤ 10.
+        /// 
+        /// 
+        /// [input] array.array.string nonogramField
+        /// 
+        /// 
+        /// A square matrix of strings of size (size + 1) / 2 + size defining the puzzle field.
+        /// The first(size + 1) / 2 cells of each row and each column define the numbers for this row/column.If there is no number in the cell, its value is "-".
+        /// The remaining size × size cells define the grid, where string "#" denotes black cells and string "." denotes white ones.
+        /// 
+        /// Guaranteed constraints:
+        /// 8 ≤ nonogramField.length ≤ 15,
+        /// nonogramField[i].length = nonogramField.length.
+        /// 
+        /// [output] boolean
+        /// 
+        /// true if the given nonogram is solved correctly and false otherwise.
+        /// </remarks>
+        public static bool correctNonogram(int size, string[][] nonogramField)
+        {
+            int n = (size + 1) / 2;
+
+            //check lines
+            for (int i = n; i < nonogramField.Length; i++)
+            {
+                var runs = nonogramField[i].Take(n).Where(c => c != "-").Select(x => int.Parse(x));
+                var hashes = string.Concat(nonogramField[i].Skip(n)).Split(".").Select(x => x.Length).Where(x => x != 0);
+
+                if (!runs.SequenceEqual(hashes))
+                    return (false);
+            }
+
+            //check columns
+            for (int j = n; j < nonogramField[0].Length; j++)
+            {
+                var col = nonogramField.Select(r => r[j]);
+                var runs = col.Take(n).Where(c => c != "-").Select(x => int.Parse(x));
+                var hashes = string.Concat(col.Skip(n)).Split(".").Select(x => x.Length).Where(x => x != 0);
+
+                if (!runs.SequenceEqual(hashes))
+                    return (false);
+            }
+
+
+            return (true);
+        }
     }
 }
