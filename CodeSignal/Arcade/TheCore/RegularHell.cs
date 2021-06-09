afab9fe2b2031364f346f603abfc47d7ccbd5b60
WanftMoon <wanft@moonlighters.com.br>
2021-06-09 09:10:59 -0300

    regular hell solutions

---
+++ CodeSignal/Arcade/TheCore/RegularHell.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CodeSignal.Arcade.TheCore
+{
+    public class RegularHell
+    {
+        /// <remarks>
+        /// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+        /// A sentence is considered correct if:
+        /// 
+        /// it starts with a capital letter;
+        ///         it ends with a full stop, question mark or exclamation point('.', '?' or '!');
+        ///         full stops, question marks and exclamation points don't appear anywhere else in the sentence.
+        /// Given a sentence, return true if it is correct and false otherwise.
+        /// 
+        /// Example
+        /// 
+        /// For sentence = "This is an example of *correct* sentence.",
+        /// the output should be
+        /// isSentenceCorrect(sentence) = true;
+        /// 
+        /// For sentence = "!this sentence is TOTALLY incorrecT",
+        /// the output should be
+        /// isSentenceCorrect(sentence) = false.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds(cs)
+        /// 
+        /// [input]
+        ///         string sentence
+        /// 
+        /// A string without newline characters.
+        /// 
+        /// Guaranteed constraints:
+        /// 2 ≤ sentence.length ≤ 100.
+        /// 
+        /// [output] boolean
+        /// 
+        /// true if the given sentence is correct, false otherwise.
+        /// </remarks>
+        public static bool IsSentenceCorrect(string sentence)
+        {
+            Regex regex = new Regex("^[A-Z][^.?!]*[.?!]$");
+            return regex.IsMatch(sentence);
+        }
+
+
+        /// <remarks>
+        /// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+        /// Implement a function that replaces each digit in the given string with a '#' character.
+        /// 
+        /// Example
+        /// 
+        /// For input = "There are 12 points", the output should be
+        /// replaceAllDigitsRegExp(input) = "There are ## points".
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds(cs)
+        /// 
+        /// [input]
+        /// string input
+        /// 
+        /// Guaranteed constraints:
+        /// 5 ≤ input.length ≤ 20.
+        /// 
+        /// [output] string
+        /// </remarks>
+        public static string ReplaceAllDigitsRegExp(string input)
+        {
+            return Regex.Replace(input, "[0-9]", "#");
+        }
+
+        /// <remarks>
+        /// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+        ///  You are given a string consisting of words separated by whitespace characters, where the words consist only of English letters.Your task is to swap pairs of consecutive words and return the result.
+        /// 
+        /// Example
+        /// 
+        /// 
+        /// For s = "CodeFight On", the output should be
+        /// swapAdjacentWords(s) = "On CodeFight";
+        ///  For s = "How are you today guys", the output should be
+        ///  swapAdjacentWords(s) = "are How today you guys".
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds(cs)
+        /// 
+        /// [input]
+        ///         string s
+        /// 
+        /// A string consisting of whitespace characters(' ') and English letters.There is exactly one whitespace character between two consecutive words, and both the first and the last characters of s are not equal to ' '.
+        /// 
+        /// Guaranteed constraints:
+        /// 0 ≤ s.length ≤ 100.
+        /// 
+        /// [output]
+        ///         string
+        /// 
+        /// String s with pairs of adjacent words swapped.
+        /// </remarks>
+        public static string SwapAdjacentWords(string s)
+        {
+            return Regex.Replace(s, "(\\w+) (\\w+)", "$2 $1");
+        }
+
+        /// <remarks>
+        /// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+        ///  You are given a string s of characters that contains at least n numbers(here, a number is defined as a consecutive series of digits, where any character immediately to the left and right of the series are not digits). The numbers may contain leading zeros, but it is guaranteed that each number has at least one non-zero digit in it.
+        /// 
+        /// Your task is to find the nth number and return it as a string without leading zeros.
+        /// 
+        /// Example
+        /// 
+        /// 
+        /// For s = "8one 003number 201numbers li-000233le number444" and n = 4,
+        /// the output should be
+        /// nthNumber(s, n) = "233".
+        /// 
+        /// Input / Output
+        /// 
+        /// [execution time limit] 3 seconds (cs)
+        /// 
+        /// [input] string s
+        /// 
+        /// 
+        /// A string containing at least n numbers.
+        /// 
+        /// 
+        /// Guaranteed constraints:
+        /// 20 ≤ s.length ≤ 650.
+        /// 
+        /// 
+        /// [input] integer n
+        /// 
+        /// 1-based index of the number to find.
+        /// 
+        /// Guaranteed constraints:
+        /// 1 ≤ n ≤ 15.
+        /// 
+        /// 
+        /// [output] string
+        /// 
+        /// The nth number without leading zeros.
+        /// </remarks>
+        public static string NthNumber(string s, int n)
+        {
+            Regex regex = new Regex(
+              @"(?:\D*0*(\d+)){" + n + "}"
+            );
+            return regex.Match(s).Groups[1].Value;
+        }
+
+        /// <remarks>
+        /// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+        /// Given a string s, determine if it is a subsequence of a given string t.
+        /// 
+        /// Example
+        /// 
+        /// 
+        /// For t = "CodeSignal" and s = "CoSi", the output should be
+        /// isSubsequence(t, s) = true;
+        /// 
+        /// For t = "CodeSignal" and s = "cosi", the output should be
+        /// the output should be
+        /// isSubsequence(t, s) = false.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds(cs)
+        /// 
+        /// [input]
+        ///         string t
+        /// 
+        /// A string consisting of English letters, whitespace characters(' '), digits and punctuation marks(".,?!=*+-").
+        /// 
+        /// Guaranteed constraints:
+        /// 0 ≤ t.length ≤ 500.
+        /// 
+        /// [input]
+        ///         string s
+        /// 
+        /// A string consisting of English letters, whitespace characters(' '), digits and punctuation marks(".,?!=*+-").
+        /// 
+        /// Guaranteed constraints:
+        /// 0 ≤ s.length ≤ 50.
+        /// 
+        /// [output] boolean
+        /// 
+        /// true if the s is a subsequence of t, false otherwise.
+        /// </remarks>
+        public static bool IsSubsequence(string t, string s)
+        {
+            string pattern = "";
+            foreach (char ch in s)
+            {
+                pattern += $"[{ch}]+.*";
+            }
+            Regex regex = new Regex(pattern);
+            return regex.Match(t).Success;
+        }
+
+        /// <remarks>
+        /// Implement the missing code, denoted by ellipses. You may not modify the pre-existing code.
+        ///  An eye rhyme is a rhyme in which two words are spelled similarly but pronounced differently.An example is the pair cough and bough; although they look similar, when they are spoken there is no rhyming quality.
+        /// 
+        /// You are writing a thesis on the eye rhyme, and you thought it would be cool to make the text itself eye rhymed.This brilliant idea came to your mind a little too late: the text is already written. Now you want to check if a given pair of lines in your text have an eye rhyme. More specifically, you want to make sure that the last three characters of each pair of lines coincide.
+        /// 
+        /// 
+        /// You have already split your text into pairs of lines. Now all that's left is to check that the last three characters of the lines in each pairOfLines coincide. Implement a function that will do this job.
+        /// 
+        /// 
+        /// Example
+        /// 
+        /// For pairOfLines = "cough\tbough", the output should be
+        /// eyeRhyme(pairOfLines) = true.
+        /// 
+        /// 
+        /// Both lines end with ugh.
+        /// 
+        /// For pairOfLines = "CodeFig!ht\tWith all your might", the output should be
+        /// eyeRhyme(pairOfLines) = false.
+        /// 
+        /// 
+        /// The first line ends with !ht, and the second one ends with ght.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds (cs)
+        /// 
+        /// [input] string pairOfLines
+        /// 
+        /// 
+        /// A string in the format "<line1>\t<line2>", where<linei> consists of at least 3 characters and may contain any character except '\t' (tabulation character). The lines are separated by '\t' (tabulation character).
+        /// 
+        /// 
+        /// Guaranteed constraints:
+        /// 7 ≤ pairOfLines.length ≤ 1000.
+        /// 
+        /// 
+        /// [output] boolean
+        /// 
+        /// true if the lines in pairOfLines have an eye rhyme, false otherwise.
+        /// </remarks>
+        public static bool eyeRhyme(string pairOfLines)
+        {
+            Regex regex = new Regex("^.*(.{3})\\t.*(.{3})$");
+            Match match = regex.Match(pairOfLines);
+            return match.Groups[1].Value == match.Groups[2].Value;
+        }
+
+
+    }
+}
--- CodeSignal/Arcade/TheCore/TimeRiver.cs
+++ CodeSignal/Arcade/TheCore/TimeRiver.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace CodeSignal.Arcade.TheCore
 {
     public class TimeRiver
@@ -326,6 +328,165 @@ namespace CodeSignal.Arcade.TheCore
             return month.ToString("MM-yyyy");
         }
 
+
+        /// <remarks>
+        /// Your Math teacher takes education very seriously, and hates it when a class is missed or canceled for any reason. He even made up the following rule: if a class is missed because of a holiday, the teacher will compensate for it with a makeup class after school.
+        /// 
+        /// You're given an array, daysOfTheWeek, with the weekdays on which your teacher's classes are scheduled, and holidays, representing the dates of the holidays throughout the academic year(from 1st of September in year to 31st of May in year + 1). How many times will you be forced to stay after school for a makeup class because of holiday conflicts in the current academic year?
+        /// 
+        /// For your convenience, here is the list of month lengths(from January to December, respectively) :
+        /// 
+        /// Month lengths: 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31.
+        /// Please note that in leap years February has 29 days.
+        /// 
+        /// Example
+        /// 
+        /// For year = 2015, daysOfTheWeek = [2, 3], and
+        /// holidays = ["11-04", "02-22", "02-23", "03-07", "03-08", "05-09"],
+        /// the output should be
+        /// missedClasses(year, daysOfTheWeek, holidays) = 3.
+        /// 
+        /// November 4th of 2015 is a Wednesday, February 23th of 2016 and March 8th of 2016 are Tuesdays, and the other holidays fall on Mondays.Classes are scheduled on Wednesdays and Tuesdays, so there will be only 3 missed classes.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds (cs)
+        /// 
+        /// [input] integer year
+        /// 
+        /// An integer representing the correct year.The current academic year started on September 1st and will finish on May 31st of year + 1.
+        /// 
+        /// Guaranteed constraints:
+        /// 1900 ≤ year ≤ 2100.
+        /// 
+        /// [input] array.integer daysOfTheWeek
+        /// 
+        /// A sorted array of integers from 1 to 7 representing the days of the week (1-based, i.e. 1 for Monday, 2 for Tuesday and so on) on which classes are scheduled.
+        /// 
+        /// Guaranteed constraints:
+        /// 0 ≤ daysOfTheWeek.length ≤ 7,
+        /// 1 ≤ daysOfTheWeek[i] ≤ 7.
+        /// 
+        /// [input] array.string holidays
+        /// 
+        /// An array of strings representing the correct dates of holidays in this academic year in the format "mm-dd", where "mm" stands for the month(1-based, i.e. "01" for January, "02" for February and so on) and "dd" stands for the day.
+        /// 
+        /// Guaranteed constraints:
+        /// 0 ≤ holidays.length ≤ 30,
+        /// 1 ≤ int(mm) ≤ 12, except 6, 7 and 8,
+        /// 1 ≤ int(dd) ≤ 31.
+        /// 
+        /// 
+        /// [output] integer
+        /// 
+        /// The number of classes that will be missed.
+        /// </remarks>
+        public static int MissedClasses(int year, int[] daysOfTheWeek, string[] holidays)
+        {
+            int missedClasses = 0;
+            DateTime classStart = new DateTime(year, 8, 1);
+
+            for (int i = 0; i < holidays.Length; i++)
+            {
+                DateTime dt = DateTime.Parse($"{year}-{holidays[i]}");
+
+                if (dt < classStart)
+                    dt = dt.AddYears(1);
+
+                int dow = (int)dt.DayOfWeek == 0 ? 7 : (int)dt.DayOfWeek;
+
+                if (daysOfTheWeek.Any(x => x == dow))
+                    missedClasses++;
+            }
+
+            return (missedClasses);
+        }
+
+        /// <remarks>
+        /// John Doe likes holidays very much, and he was very happy to hear that his country's government decided to introduce yet another one. He heard that the new holiday will be celebrated each year on the xth week of month, on weekDay.
+        /// 
+        /// Your task is to return the day of month on which the holiday will be celebrated in the year yearNumber.
+        /// 
+        /// For your convenience, here are the lists of months names and lengths and the list of days of the week names.
+        /// 
+        /// Months: "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December".
+        /// Months lengths: 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31.
+        /// Days of the week: "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday".
+        /// Please, note that in leap years February has 29 days.
+        /// 
+        ///  Example
+        /// 
+        ///  For x = 3, weekDay = "Wednesday", month = "November", and yearNumber = 2016, the output should be
+        ///  holiday(x, weekDay, month, yearNumber) = 16.
+        /// 
+        ///  The new holiday will be celebrated every November on the 3rd Wednesday of the month.In 2016 the 3rd Wednesday falls on the 16th of November.
+        /// 
+        /// For x = 5, weekDay = "Thursday", month = "November", and yearNumber = 2016, the output should be
+        /// holiday(x, weekDay, month, yearNumber) = -1.
+        /// 
+        /// There are only 4 Thursdays in November 2016.
+        /// 
+        /// Input/Output
+        /// 
+        /// [execution time limit] 3 seconds(cs)
+        /// 
+        /// [input]
+        ///         integer x
+        /// 
+        /// A positive integer.
+        /// 
+        /// Guaranteed constraints:
+        /// 1 ≤ x ≤ 5.
+        /// 
+        /// [input]
+        ///         string weekDay
+        /// 
+        /// A string representing a correct name of some day of the week.
+        /// 
+        /// Guaranteed constraints:
+        /// weekDay ∈ {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}.
+        /// 
+        /// [input]
+        ///         string month
+        /// 
+        /// A string representing a correct name of some month.
+        /// 
+        /// Guaranteed constraints:
+        /// month ∈ { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}.
+        /// 
+        /// [input]
+        ///         integer yearNumber
+        /// 
+        /// Guaranteed constraints:
+        /// 2015 ≤ yearNumber ≤ 2500.
+        /// 
+        /// [output]
+        ///         integer
+        /// 
+        /// The day of month on which the holiday will be celebrated in the year yearNumber.If there is no answer, return -1.
+        /// </remarks>
+        public static int Holiday(int x, string weekDay, string month, int yearNumber)
+        {
+            string[] weekDays = new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            string[] months = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            DateTime current = new DateTime(yearNumber, Array.IndexOf(months, month) + 1, 1);
+            int count = 0;
+
+            while (true)
+            {
+                if ((int)current.DayOfWeek == Array.IndexOf(weekDays, weekDay))
+                    count++;
+
+                if (count == x)
+                    break;
+
+                current = current.AddDays(1);
+            }
+
+            Console.WriteLine($"{current} {count}");
+
+            return (Array.IndexOf(months, month) + 1 == current.Month ? current.Day : -1);
+        }
 
     }
 }
